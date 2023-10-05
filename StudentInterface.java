faa882bb223b724073e28771da5fd5ed19d086ee
Bryce Lam <bcllamc280@gmail.com>
2023-10-05 10:48:01 -0700

    Update StudentInterface.java

--- StudentInterface.java
+++ StudentInterface.java
@@ -1,18 +1,14 @@
 // Allow students to submit answers. Every student has a unique ID (String), and can
 // submit an answer to the iVote service.
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
 
 public interface StudentInterface
 {
-    /** Get the student's name
-     * @return Student's name
-     */
-    public String getName();
-
     /** Get the student's ID
      * @return Student's ID
      */
-    public String getID();
+    public UUID getID();
 
     /** Get the student's one answer
      * @return a ArrayList of the student's answers
@@ -22,7 +18,7 @@ public interface StudentInterface
     /** Get the student's answers
      * @return a ArrayList of the student's answers
      */
-    public ArrayList<String> getAnswers();
+    public Set<String> getAnswers();
 
     /** Adds an answer to the student's answer for the single choice question
      * @param answer The answer to add
@@ -32,5 +28,5 @@ public interface StudentInterface
     /** Adds list of answer to the student's answers for the multiple choice question
      * @param answer The answer to add
      */
-    public void submitAnswer(ArrayList<String> answer);
+    public void submitAnswer(Set<String> answer);
 }
