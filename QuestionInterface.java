e6ffbaed4e9fdeffafff7bce102729ff1bc627b7
Bryce Lam <bcllamc280@gmail.com>
2023-10-05 11:59:14 -0700

    Update QuestionInterface.java

--- QuestionInterface.java
+++ QuestionInterface.java
@@ -7,17 +7,17 @@ public interface QuestionInterface
 	 */
     public String getQuestion();
     
-    /** Get a list of all possible choices to the question
-     * @return a ArrayList of the answers
+    /** Get all of the possible answers to the question
+     * @return a set of the answers
      */
     public abstract Set<String> getAllAnswers();
 
-    /** Get a list of the correct answer(s) to the question
-     * @return a ArrayList of the answers
+    /** Get all of the correct answer(s) to the question
+     * @return a set of the answers
      */
     public abstract Set<String> getCorrectAnswers();
 
-    /** Adds the answer to the question as a key and value
+    /** Adds the answer to question's answer set as a key and value
      * @param answer Key: String answer
      * @param correct Value: True if the answer is correct
      */
