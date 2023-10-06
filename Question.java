2ebe2d7cb90980779cd0cba1ce2114911669e4f1
Bryce Lam <bcllamc280@gmail.com>
2023-10-06 10:56:34 -0700

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,14 +1,21 @@
 public abstract class Question implements QuestionInterface
 {
     private String question;
+    private String questionType;
     
-    public Question(String question)
+    public Question(String question, String questionType)
     {
         this.question = question;
+        this.questionType = questionType;
     }
 
     public String getQuestion()
     {
         return question;
+    }
+
+    public String getQuestionType()
+    {
+        return questionType;
     }
 }
